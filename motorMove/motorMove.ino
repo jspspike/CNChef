db67f15ed77fa781dc053618bf10e9e394d6a82a
jspspike <jspspike@gmail.com>
2016-05-09 14:15:41 -0500

    Removed motor 2 and servo and set up for new servos

--- motorMove/motorMove.ino
+++ motorMove/motorMove.ino
@@ -5,102 +5,70 @@ int MOTOR1_DIR1 = 2;
 int MOTOR1_DIR2 = 3;
 int MOTOR1_SPEED = 9;
 
-int MOTOR2_DIR1 = 4;
-int MOTOR2_DIR2 = 5;
-int MOTOR2_SPEED = 10;
+int BUTTON_1 = 5;
+int BUTTON_2 = 6;
+int BUTTON_3 = 7;
 
-int BUTTON_1 = 6;
-int BUTTON_2 = 7;
+int SERVO_1 = 0;
+int SERVO_2 = 1;
+int SERVO_3 = 4;
 
-Servo servo1;
 
-void dropMili(int mil) {
-  mil /= 1.16;
-  servo1.write(125);
-  delay(1000);
-  servo1.write(180);
-}
+bool active = false;
 
 void setup() {
-  //pinMode(11, OUTPUT);
-  Serial.begin(9600);
   
-  servo1.attach(11);
+  Serial.begin(9600);
   
   pinMode(BUTTON_1, INPUT);
   pinMode(BUTTON_2, INPUT);
+  pinMode(BUTTON_3, INPUT);
+
+  pinMode(SERVO_1, OUTPUT);
+  pinMode(SERVO_2, OUTPUT);
+  pinMode(SERVO_3, OUTPUT);
 
   pinMode(MOTOR1_DIR1, OUTPUT);
   pinMode(MOTOR1_DIR2, OUTPUT);
   pinMode(MOTOR1_SPEED, OUTPUT);
-  pinMode(MOTOR2_DIR1, OUTPUT);
-  pinMode(MOTOR2_DIR2, OUTPUT);
-  pinMode(MOTOR2_SPEED, OUTPUT);
+
   
-  servo1.write(180);
+  
+  moveLeft(255);
+  delay(3000);
+  stopMotors();
+}
+
+void moveLeftTime(int time, int speed) {
+  moveLeft(speed);
+  delay(time);
+  stopMotors();
+}
+
+void moveRightTime(int time, int speed) {
+  moveRight(speed);
+  delay(time);
+  stopMotors();
 }
 
 void moveLeft(int speed) {
     analogWrite(MOTOR1_SPEED, speed);
     digitalWrite(MOTOR1_DIR1, LOW);
     digitalWrite(MOTOR1_DIR2, HIGH);
-
-    analogWrite(MOTOR2_SPEED, speed);
-    digitalWrite(MOTOR2_DIR1, LOW);
-    digitalWrite(MOTOR2_DIR2, HIGH);
 }
 
 void moveRight(int speed) {
     analogWrite(MOTOR1_SPEED, speed);
     digitalWrite(MOTOR1_DIR1, HIGH);
     digitalWrite(MOTOR1_DIR2, LOW);
-
-    analogWrite(MOTOR2_SPEED, speed);
-    digitalWrite(MOTOR2_DIR1, HIGH);
-    digitalWrite(MOTOR2_DIR2, LOW);
 }
 
 void stopMotors() {
     analogWrite(MOTOR1_SPEED, 0);
     digitalWrite(MOTOR1_DIR1, LOW);
     digitalWrite(MOTOR1_DIR2, HIGH);
-
-    analogWrite(MOTOR2_SPEED, 0);
-    digitalWrite(MOTOR2_DIR1, LOW);
-    digitalWrite(MOTOR2_DIR2, HIGH);
 }
 
 void loop() {
 
-  if (digitalRead(BUTTON_1) == LOW && digitalRead(BUTTON_2) == LOW) {
-    servo1.write(125);
-    delay(200);
-    while (!(digitalRead(BUTTON_1) == LOW && digitalRead(BUTTON_2) == LOW)) {
-      delay(10);
-      stopMotors();
-    }
-    servo1.write(180);
-    delay(200);
-  }
-
-  else if (digitalRead(BUTTON_1) == LOW && digitalRead(BUTTON_2) == HIGH) {
-    moveRight(255);
-  }
-
-  else if (digitalRead(BUTTON_1) == HIGH && digitalRead(BUTTON_2) == LOW) {
-    moveLeft(255);
-  }
-
-  else {
-    stopMotors();
-  }
-
-
-  
-//  if (digitalRead(2) == LOW) {
-//    servo1.write(125);  
-//  }
-//  if (digitalRead(3) == LOW) {
-//    servo1.write(180);
-//  } 
 }
---
+++ motorMove/servo.ino/servo.ino.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+Servo servo1;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  servo1.attach(9);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  servo1.write(130);
+}
